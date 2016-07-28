8b97ad386591d7e41e5a3e102972e683dfaaaa3e
Matthew Casiro <mattcasiro@gmail.com>
2016-07-28 16:49:56 -0700

    Scraper should return the correct output for any clue requested

--- spec/jeopardy/scraper_spec.rb
+++ spec/jeopardy/scraper_spec.rb
@@ -38,10 +38,6 @@ module Jeopardy
       end
 
       context "the final round" do
-        let :categories do
-          categories
-        end
-
         it "has one category" do
           expect(subject.rounds.last.categories.count).to eql(1)
         end
@@ -53,14 +49,50 @@ module Jeopardy
         end
       end
 
-      context "an assortment of specific categories and clues" do
-        it "match the expected output" do
+      context "an assortment of categories" do
+        it "are correctly named" do
           expect(subject.rounds[0].categories[0].name).to eql("LAKES & RIVERS")
           expect(subject.rounds[0].categories[3].name).to eql("FOREIGN CUISINE")
           expect(subject.rounds[1].categories[2].name).to eql("NATIONAL LANDMARKS")
           expect(subject.rounds[1].categories[5].name).to eql("4-LETTER WORDS")
+          expect(subject.rounds[2].categories[0].name).to eql("HOLIDAYS")
+        end
+      end
+
+      context "an assortment of clues" do
+        it "provide the correct output" do
+          q_and_a_hash = {}
+          2.times do |i|
+            6.times do |j|
+              q_and_a_hash["R#{i}-CA#{j}-CL#{j%5}"] = [subject.rounds[i].categories[j].clues[j%5].question, subject.rounds[i].categories[j].clues[j%5].answer]
+            end
+          end
+          expected = {
+            "R0-CA0-CL0"=>["the Jordan", "River mentioned most often in the Bible"],
+            "R0-CA1-CL1"=>["the rickshaw", "In 1869 an American minister created this \"oriental\" transportation"],
+            "R0-CA2-CL2"=>["a weasel", "When husbands \"pop\" for an ermine coat, they\\'re actually buying this fur"],
+            "R0-CA3-CL3"=>["ChÃ¢teaubriand", "French for a toothsome cut of beef served to a twosome"],
+            "R0-CA4-CL4"=>["Colonel Chuck Yeager", "Sam Shepard played this barrier breaker in \"The Right Stuff\""],
+            "R0-CA5-CL0"=>[nil, nil],
+            "R1-CA0-CL0"=>["the walls", "When \"Joshua Fit The Battle Of Jericho\", these took a tumble"],
+            "R1-CA1-CL1"=>["Eve Arden", "She was \"Our Miss Brooks\""],
+            "R1-CA2-CL2"=>["Plymouth Rock", "The cornerstone of Massachusetts, it bears the date 1620"],
+            "R1-CA3-CL3"=>[nil, nil],
+            "R1-CA4-CL4"=>["John Wilkes Booth", "After the deed, he leaped to the stage shouting \"Sic semper tyrannis\""],
+            "R1-CA5-CL0"=>["shot", "Pulled the trigger or what\\'s in a jigger"]
+          }
+          expect(q_and_a_hash).to eql(expected)
         end
       end
     end
+
+    # Test game without round 2
+    context "A game without a round 2" do
+      subject do
+        scraper.new_game!(BASE_PATH + "show_317_id_4256.html")
+      end
+    end
+
+
   end
 end
