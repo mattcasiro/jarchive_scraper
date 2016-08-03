ab79cbc6d29e6354246de78a5c9d903494a8e16e
Matthew Casiro <mattcasiro@gmail.com>
2016-08-02 17:44:06 -0700

    Clues should be comparable

--- lib/jeopardy/clue.rb
+++ lib/jeopardy/clue.rb
@@ -2,12 +2,17 @@ require 'pry'
 
 module Jeopardy 
   class Clue
+    include Comparable
     attr_accessor :question, :answer, :value
     
     def initialize(q, a, v=0)
       @question = q
       @answer = a
       @value = v
+    end
+
+    def <=>(other)
+      self.value <=> other.value
     end
   end
 end
